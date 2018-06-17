c958ab06bd2124c23edd9e21fa6f66148662970c
Alexander Cochrane <dontdropmybass@gmail.com>
2018-06-17 12:54:27 -0300

    did I change anything?

--- cars.php
+++ cars.php
@@ -34,7 +34,7 @@ include("options.php");
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Car flip cards</title>
+<title>Dannylist: Buy and Sell Cars Online</title>
 <link href='cars.css' rel='stylesheet' />
 
 </head>
